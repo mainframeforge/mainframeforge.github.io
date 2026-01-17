c77e16b6140ce2ce260c18540e967a9389063821
Dušan Mitrović <dusan.mitrovic.developer@gmail.com>
2026-01-18 00:12:06 +0100

    feat(links): implement dynamic experience calculation (ref #10)

--- js/links.js
+++ js/links.js
@@ -11,11 +11,13 @@
 const ALERT_POPUP_ID = 'alert-popup-id';
 const DISCORD_BTN_ID = 'discord-btn';
 const EMAIL_BTN_ID = 'email-btn';
+const EXPERIENCE_METRIC_ID = 'experience-metric';
 
 const DISCORD_ALERT_LABEL = 'Discord Username';
 const EMAIL_ALERT_LABEL = 'Email Address';
 
 const ON_ALERT_EVENT = 'click';
+const ON_DOM_CONTENT_LOADED = 'DOMContentLoaded';
 
 const HIDE_TIMEOUT_MS = 300;
 const FLY_OUT_TIMEOUT_MS = 2000;
@@ -28,6 +30,9 @@ const FLY_OUT_STYLE_ANIMATION = 'flyOutRight 0.3s ease forwards';
 const COPIED_LABEL = ' Copied!';
 const ERROR_PREFIX_TEXT = 'Error occurred: ';
 const ALERT_COPY_FAILED_TEXT = 'Copy failed :(';
+
+const START_EXPERIENCE_YEAR = 2017;
+const EXPERIENCE_METRIC_SUFFIX_TEXT = "+ Years Experience";
 
 let flyOutTimer;
 let hideTimer;
@@ -89,6 +94,34 @@ async function onBtnClicked(elementId, label) {
   animateAlert(alertNotice);
 }
 
+/**
+ * Calculates professional years of experience.
+ * @param {number} startYear The year career began.
+ * @return {number} The calculated years of experience.
+ */
+function calculateExperience(startYear) {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  return currentYear - startYear;
+}
+
+/**
+ * Injects dynamic experience metrics into the DOM.
+ * @param {string} elementId The ID of the experience metric element.
+ * @param {number} startYear The year career began.
+ * @param {string} suffixText Text to be displayed after metric number.
+ * @return {void}
+ */
+function updateExperienceMetric(elementId, startYear, suffixText) {
+  const experienceMetricElement = document.getElementById(elementId);
+  if (!experienceMetricElement) {
+    return;
+  }
+
+  const years = calculateExperience(startYear);
+  experienceMetricElement.innerHTML = `${years}${suffixText}`;
+}
+
 const discordBtn = document.getElementById(DISCORD_BTN_ID);
 const emailBtn = document.getElementById(EMAIL_BTN_ID);
 
@@ -98,3 +131,9 @@ registerEventListener(discordBtn, ON_ALERT_EVENT, () => {
 registerEventListener(emailBtn, ON_ALERT_EVENT, () => {
   onBtnClicked(EMAIL_BTN_ID, EMAIL_ALERT_LABEL);
 });
+registerEventListener(document, ON_DOM_CONTENT_LOADED, () => {
+  updateExperienceMetric(
+    EXPERIENCE_METRIC_ID,
+    START_EXPERIENCE_YEAR,
+    EXPERIENCE_METRIC_SUFFIX_TEXT);
+})
